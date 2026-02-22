0e29a055de5e3da8a6cabc6d78b6d1481fa15dc9
Joan Diaz Estigarribia <joan.diaz094@gmail.com>
2026-02-22 13:19:24 -0300

    fix: set global API prefix for application routes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -72,6 +72,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
+  app.setGlobalPrefix('api');
+
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
   
